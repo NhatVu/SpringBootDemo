8be6ad75ca8125490638028edb432ad45c018a4d
NhatVu <minhnhatse.uit@gmail.com>
2023-07-03 15:16:58 +0100

    add proptotype pattern

---
+++ src/main/java/com/example/demo/pattern/creation/prototype/PrototypeDemo.java
@@ -0,0 +1,55 @@
+package com.example.demo.pattern.creation.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Prototype is a creational design pattern that lets you copy existing objects without making your code dependent on their classes.
+
+You can use new operator but Not all objects can be copied that way because some of the object’s fields may be private
+and not visible from outside of the object itself.
+ */
+public class PrototypeDemo {
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        rectangle.color = "blue";
+        shapes.add(rectangle);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+
+    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": But they are not identical (booo!)");
+                }
+            } else {
+                // because we copy object -> no object will have same reference -> will not go to this block
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+    }
+}
